16952a40c27ad800045b07ad860288d17bfa88b1
BeamzXD <makes.k.s@mail.ru>
2024-05-03 22:46:05 +0300

    edited

--- _build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/beamz/BeamzXD/workspace/projects/lab05
-# Build directory: /Users/beamz/BeamzXD/workspace/projects/lab05/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/Users/beamz/BeamzXD/workspace/projects/lab05/_build/check")
-set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/Users/beamz/BeamzXD/workspace/projects/lab05/CMakeLists.txt;19;add_test;/Users/beamz/BeamzXD/workspace/projects/lab05/CMakeLists.txt;0;")
-subdirs("third-party/gtest")
